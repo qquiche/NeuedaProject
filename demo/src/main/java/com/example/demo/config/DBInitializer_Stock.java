d8b0ddf2629777d879d333080788c0da9e5ca3c4
Kelvin Bueno <kelvinbueno@Kelvins-MacBook-Pro.local>
2024-06-13 08:38:57 -0500

    added dummy data

--- demo/src/main/java/com/example/demo/config/DBInitializer_Stock.java
+++ demo/src/main/java/com/example/demo/config/DBInitializer_Stock.java
@@ -22,7 +22,15 @@ public class DBInitializer_Stock {
                 // TODO: Add more dummy data
                     new Stock(1, "Apple Inc.", "AAPL", 125.91),
                     new Stock(2, "Microsoft Corporation", "MSFT", 250.78),
-                    new Stock(3, "Amazon.com Inc.", "AMZN", 3286.58)
+                    new Stock(3, "Amazon.com Inc.", "AMZN", 3286.58),
+                    new Stock(4, "Alphabet Inc.", "GOOGL", 2345.10),
+                    new Stock(5, "Facebook Inc.", "FB", 329.51),
+                    new Stock(6, "Tesla Inc.", "TSLA", 661.75),
+                    new Stock(7, "NVIDIA Corporation", "NVDA", 532.00),
+                    new Stock(8, "PayPal Holdings Inc.", "PYPL", 250.00),
+                    new Stock(9, "Netflix Inc.", "NFLX", 500.00),
+                    new Stock(10, "Adobe Inc.", "ADBE", 500.00),
+                    new Stock(11, "Zoom Video Communications Inc.", "ZM", 500.00)
             );
 
             // Save all users to the database
